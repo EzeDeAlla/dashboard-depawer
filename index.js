d269c27c2131cc8c8884364e80c5ad8cd324e93b
Ezequiel Martinez <ezeelmartinez@gmail.com>
2022-11-16 19:45:08 -0300

    script star change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './src/reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import { store } from './src/redux/store/index';
-
+ // aa
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
